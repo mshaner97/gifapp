aba23aed0fcd882d6a247129871e4261e24cafaa
mshaner97 <mshaner97@icloud.com>
2025-02-11 19:45:19 -0600

    fixes for testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import { describe, it, expect } from 'vitest'
 
 describe('App', () => {
   it('renders the App component', () => {
